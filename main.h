5d4b5a9cd5bce3a4d7993e5ae3fb8f0b43ab772d
Mmaureeny <muthonimaureen4@outlook.com>
2022-04-19 05:41:07 -0700

    printf

--- main.h
+++ main.h
@@ -1,8 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
+
 
 int _printf(const char *format, ...);
+char *print_bin(va_list list);
 
 #endif
