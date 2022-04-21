6008ce724ac112e724ae234ce5e3e6393437c3bd
AUSTINEODUOR <austineoduor57@gmail.com>
2022-04-21 05:20:50 -0700

    file

--- get_func.c
+++ get_func.c
@@ -7,20 +7,21 @@
  */
 int (*get_func(char x))(va_list)
 {
-        int i = 0;
-        spec arr[] = {
-                {"c", print_c},
-                {"s", print_s},
-                {"%", print_percent},
-                {"d", print_d},
-                {"i", print_i},
-                {NULL, NULL}
-        };
-        while (arr[i].valid)
-        {
-                if (x == arr[i].valid[0])
-                        return (arr[i].f);
-                i++;
-        }
-        return (NULL);
+	int i = 0;
+
+	spec arr[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"%", print_percent},
+		{"d", print_d},
+		{"i", print_i},
+		{NULL, NULL}
+	};
+	while (arr[i].valid)
+	{
+		if (x == arr[i].valid[0])
+			return (arr[i].f);
+		i++;
+	}
+	return (NULL);
 }
